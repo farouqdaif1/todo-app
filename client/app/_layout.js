49b2378f7e1adfaa87eca94266fff25b7e5430b7
farouqdaif1 <daiffarouq@gmail.com>
2024-01-18 18:22:03 +0200

    Setup expo

--- client/app/_layout.js
+++
@@ -1,7 +0,0 @@
-import { Stack } from 'expo-router';
-const Layout = ({ children }) => {
-    return (
-        <Stack />
-    );
-};
-export default Layout;
--- client/app/index.js
+++
@@ -1,9 +0,0 @@
-import { View, Text, SafeAreaView } from "react-native";
-const Home = () => {
-    return (
-        <SafeAreaView>
-
-        </SafeAreaView>
-    );
-}
-export default Home;
